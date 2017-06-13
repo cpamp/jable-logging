349f2412e6132c3309302be0028031fbf708c341
Pamperin, Clint A <c.pamperin21@gmail.com>
2017-06-13 17:41:41 -0500

    refactor: allow changing output

--- src/Log.ts
+++ src/Log.ts
@@ -1,10 +1,20 @@
-export function Log(pipe: (out: string) => void = console.log) {
+export type IOutLogString = (construcotrName: string, propertyKey: string) => string;
+
+function defaultStart(constructorName: string, propertyKey: string): string {
+    return constructorName + '.' + propertyKey + ' started at ' + new Date(Date.now()).toUTCString();
+}
+
+function defaultEnd(constructorName: string, propertyKey: string): string {
+    return constructorName + '.' + propertyKey + ' ended at ' + new Date(Date.now()).toUTCString();
+}
+
+export function Log(pipe: (out: string) => void = console.log, start: IOutLogString = defaultStart, end: IOutLogString = defaultEnd) {
     return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
         var oldFunc = target[propertyKey];
         descriptor.value = function() {
-            pipe(target.constructor.name + '.' + propertyKey + ' started at ' + new Date(Date.now()).toUTCString());
+            pipe(start(target.constructor.name, propertyKey));
             var result = oldFunc.apply(this, arguments);
-            pipe(target.constructor.name + '.' + propertyKey + ' ended at ' + new Date(Date.now()).toUTCString());
+            pipe(end(target.constructor.name, propertyKey));
             return result;
         }
     };
--- src/LogError.ts
+++ src/LogError.ts
@@ -1,11 +1,17 @@
-export function Log(pipe: (out: string) => void = console.error, reThrow: boolean = true) {
+type IOutLogErrorString = (constructorName: string, propertyKey: string, err: any) => string
+
+function defaultErrorLog(constructorName: string, propertyKey: string, err: any): string {
+    return constructorName + '.' + propertyKey + ' threw error at ' + new Date(Date.now()).toUTCString() + ': ' + err;
+}
+
+export function LogError(pipe: (out: string) => void = console.error, errorLog: IOutLogErrorString = defaultErrorLog, reThrow: boolean = true) {
     return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
         var oldFunc = target[propertyKey];
         descriptor.value = function() {
             try {
                 var result = oldFunc.apply(this, arguments);
             } catch(e) {
-                pipe(target.constructor.name + '.' + propertyKey + ' threw error at ' + new Date(Date.now()).toUTCString() + ': ' + e);
+                pipe(errorLog(target.constructor.name, propertyKey, e));
                 if (reThrow) throw e;
             }
             return result;
