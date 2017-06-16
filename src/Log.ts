328f95158dc94419f2352262eca4cdd93f849731
Pamperin, Clint A <c.pamperin21@gmail.com>
2017-06-15 22:57:13 -0500

    refactor: start and end functions to accept args and result

--- src/Log.ts
+++ src/Log.ts
@@ -1,4 +1,5 @@
-export type IOutLogString = (construcotrName: string, propertyKey: string) => string;
+export type IStartLogString = (construcotrName: string, propertyKey: string, args: IArguments) => string;
+export type IEndLogString = (construcotrName: string, propertyKey: string, result: any) => string;
 
 function defaultStart(constructorName: string, propertyKey: string): string {
     return constructorName + '.' + propertyKey + ' started at ' + new Date(Date.now()).toUTCString();
@@ -8,17 +9,18 @@ function defaultEnd(constructorName: string, propertyKey: string): string {
     return constructorName + '.' + propertyKey + ' ended at ' + new Date(Date.now()).toUTCString();
 }
 
-export function Log(pipe: (out: string) => void = console.log, start: IOutLogString = defaultStart, end: IOutLogString = defaultEnd) {
+export function Log(pipe: (out: string) => void = console.log, start: IStartLogString = defaultStart, end: IEndLogString = defaultEnd) {
     return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
         var oldFunc = descriptor.value;
         descriptor.value = function() {
-            pipe(start(target.constructor.name, propertyKey));
+            var result;
+            pipe(start(target.constructor.name, propertyKey, arguments));
             try {
-                var result = oldFunc.apply(this, arguments);
+                result = oldFunc.apply(this, arguments);
             } catch(e) {
                 throw e;
             } finally {
-                pipe(end(target.constructor.name, propertyKey));
+                pipe(end(target.constructor.name, propertyKey, result));
             }
             return result;
         }
